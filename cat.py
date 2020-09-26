402f47163de1e1a31cae17888b31c142d39195b4
daliborstakic <stakicdalibor64@gmail.com>
2020-09-26 10:59:08 +0200

    Add Tkinter UI with button command
    This way I can easily generate a random fact using a button click.

--- cat.py
+++ cat.py
@@ -1,5 +1,18 @@
 """ Importing module requests """
 import requests
+""" Import Tkinter objects """
+from tkinter import Tk, Button, Label
+
+class AppUI():
+    def __init__(self, master, button_command):
+        """ Init method """
+        self.master = master
+        self.label = Label(master, text="Press for a random fact")
+        self.button = Button(master, text="Fact", command=button_command)
+
+        # Grid system
+        self.label.grid(row=0, column=0, padx=5, pady=5)
+        self.button.grid(row=1, column=0, padx=5, pady=5)
 
 # API url
 url = "https://cat-fact.herokuapp.com/facts/random"
